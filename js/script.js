1a58e7d0bc79c974aef53c82f0bc2e4443daecf6
antoniovenezia <antoniovenezia00@gmail.com>
2021-06-01 14:06:34 +0200

    js-fizzbuzz

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 for (var i = 1; i <= 100; i++) {
     
-    if (i % 3 == 0) {
+    if (i % 15 == 0) {
+        document.getElementById("game").innerHTML += ("FizzBuzz" + "</br>");
+    } else if(i % 3 == 0) {
         document.getElementById("game").innerHTML += ("fizz" + "</br>") ;
     } else if (i % 5 == 0) {
         document.getElementById("game").innerHTML += ("buzz" + "</br>") ;
-    } 
-    document.getElementById("game").innerHTML += (i + "</br>");
+    } else {
+        document.getElementById("game").innerHTML += (i + "</br>");
+    }
 }
-
-
-console.log();
-
 
